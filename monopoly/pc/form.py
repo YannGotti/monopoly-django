3c01b284608c9273dd274eec8f3a2fdc467aa20d
mironovv <1111111111aleksey46@gmail.com>
2023-01-20 10:20:42 +0300

    add c++ project

--- monopoly/pc/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UserPc
-
-class PCForms(forms.ModelForm):
-    class Meta:
-        model = UserPc
-        fields = ('name', 'ip', 'mac_adress', 'description')
